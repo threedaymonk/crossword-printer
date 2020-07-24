371130376fa8100e13be924af75bf658d31d719c
Paul Battley <pbattley@gmail.com>
2020-07-24 10:06:33 +0100

    Generate LaTeX output for a PDF

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const libxmljs = require("libxmljs");
 const fs = require("fs");
 const _ = require("lodash");
+const { stripIndents } = require("common-tags");
 
 const NAMESPACES = {
   c: "http://crossword.info/xml/crossword-compiler",
@@ -10,7 +11,8 @@ const NAMESPACES = {
 const main = () => {
   for (const path of process.argv.slice(2)) {
     const xml = fs.readFileSync(path);
-    console.log(JSON.stringify(parseXml(xml), null, 2));
+    const crossword = parseXml(xml);
+    console.log(generateTex(crossword));
   }
 };
 
@@ -60,13 +62,63 @@ const extractClues = (crossword) => {
     .reduce((result, clues) => {
       const words = clues.find("p:clue", NAMESPACES).reduce((words, clue) => {
         const number = parseInt(clue.attr("number").value(), 10);
-        const text = `${clue.text().trim()} (${clue.attr("format").value()})`;
-        return { ...words, [number]: text };
-      }, {});
+        const text = clue.text().trim();
+        const format = clue.attr("format").value();
+        return [...words, [number, text, format]];
+      }, []);
       const title = clues.get("p:title", NAMESPACES).text().trim();
-      return { ...result, [title]: words };
-    }, {});
+      return [...result, [title, words]];
+    }, []);
   return { clues: result };
+};
+
+const generatePuzzle = (crossword) => {
+  let output = `\\begin{Puzzle}{${crossword.width}}{${crossword.height}}%\n`;
+  crossword.cells.forEach((row) => {
+    row.forEach((cell) => {
+      let buf = "|";
+      if (cell.type == "letter") {
+        if (cell.number) buf += `[${cell.number}]`;
+        buf += cell.solution;
+      } else {
+        buf += "*";
+      }
+      output += buf.padEnd(7, " ");
+    });
+    output += "|.\n";
+  });
+  output += "\\end{Puzzle}\n";
+  return output;
+};
+
+const generateClues = (crossword) => {
+  let output = "";
+  crossword.clues.forEach(([dir, clues]) => {
+    output += `\\section*{${dir}}\n\n\\begin{itemize}\n`;
+    clues.forEach(([number, clue, format]) => {
+      output += `\\item [${number}] ${clue} (${format})\n\n`;
+    });
+    output += `\\end{itemize}\n`;
+  });
+  return output;
+};
+
+const generateTex = (crossword) => {
+  return stripIndents`
+    \\documentclass{article}
+    \\usepackage{fontspec}
+    \\usepackage{xunicode}
+    \\setmainfont{Lato}
+    \\usepackage[margin=1in]{geometry}
+    \\usepackage[large]{cwpuzzle}
+    \\begin{document}
+
+    ${generatePuzzle(crossword)}
+
+    ${generateClues(crossword)}
+
+    \\end{document}
+  `;
 };
 
 main();
